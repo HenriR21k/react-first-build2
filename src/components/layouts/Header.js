3b5ccc6c8f597850892c65620041141d0c7bce7c
HenriR21k <106344477+HenriR21k@users.noreply.github.com>
2022-06-08 18:42:31 +0100

    added router, routes, route and NavLink and Link react-router-dom components (#2)

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,3 +1,4 @@
+import {Link } from 'react-router-dom';
 import './Header.css';
 
 function Header() {
@@ -9,14 +10,14 @@ function Header() {
 
     return (
         <header>
-            <a>
+            <Link to="/">
                 <img src = "https://img.icons8.com/bubbles/344/bbb.png"/>
-            </a>
-            <a>
+            </Link>
+            <Link to="/">
                 <h1>React First Build</h1>
-            </a>
+            </Link>
             <div className = "login">
-                <p>Welcome Graeme!</p>
+                <p>Welcome Henri!</p>
             </div>
         </header>
 
--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,22 +1,27 @@
+import { NavLink } from 'react-router-dom';
 import './Navbar.css';
+
 
 function Navbar() {
     // Properties ----------------------------
     // Hooks ---------------------------------
     // Context -------------------------------
     // Methods -------------------------------
+
+    const getLinkStyle = ({ isActive }) => ( isActive ? 'navSelected' : null);
+
     // View ---------------------------------
 
     return (
         <nav>
             <div className="navItem">
-                <a href='/'>Home</a>
+                <NavLink to='/' className={getLinkStyle}>Home</NavLink>
             </div>
             <div className="navItem">
-                <a href='/signin'>Sign In</a>
+                <NavLink to='/signin' className={getLinkStyle}>Sign In</NavLink>
             </div>
             <div className="navItem">
-                <a href='/contact'>Contact us</a>
+                <NavLink to='/contact' className={getLinkStyle}>Contact us</NavLink>
             </div>
         </nav>
 
