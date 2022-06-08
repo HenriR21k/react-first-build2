e5515e0a10e3504b477eb7287cc93a913df3a381
HenriR21k <106344477+HenriR21k@users.noreply.github.com>
2022-06-08 17:42:49 +0100

    Added page and layout components (#1)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import Layout from './components/layouts/Layout.js'
+import Home from './components/pages/Home.js';
+import SignIn from './components/pages/SignIn.js';
+import ContactUs from './components/pages/ContactUs';
+import PageNotFound from './components/pages/404.js';
+
+
+import './App.css';
+
 
 
 function App() {
   return (
-    <p>Hello World!</p>
+    <Layout>
+      <Home />
+    </Layout>
 
   );
 }
