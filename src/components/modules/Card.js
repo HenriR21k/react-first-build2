03e9ea28848d4fbc0fc21bb791841d5eb23f61dd
HenriR21k <k2017731@kingston.ac.uk>
2022-06-16 15:46:08 +0100

    fixed css issues

--- src/components/modules/Card.js
+++ src/components/modules/Card.js
@@ -8,11 +8,11 @@ function Card({module}) {
             </div>
             <div className="content">
                 <h1>{module.ModuleName}</h1>
-                <p>{module.ModuleCode}</p>
-                <p>{module.ModuleLevel}</p>
+                <p>Module Code: {module.ModuleCode}</p>
+                <p>Module Level: {module.ModuleLevel}</p>
             </div>
             <div className="actions">
-                <button>To Favourites</button>
+                <button>Add To Favourites</button>
             </div>
         </div>
     );
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,6 +6,8 @@ import { ModuleList } from '../modules/modulesList.js';
 
 
 
+
+
 function Home() {
     // Properties ----------------------------
     // Hooks ---------------------------------
@@ -15,7 +17,13 @@ function Home() {
 
     return ( <section>
 
-        <h1>All Modules</h1>
+        <div className="heading">All Modules</div>
+
+        <div className = "favouriteButton">
+            <button> <img class = "heart" src ="https://cdn-icons.flaticon.com/png/512/2589/premium/2589175.png?token=exp=1655390263~hmac=70dae7f22c500d0cc77ad17858ce2aee" alt = "heart"/> See Favourites?</button>
+        </div>
+        
+        
     
         <ModuleList modules={modules} /> 
        
