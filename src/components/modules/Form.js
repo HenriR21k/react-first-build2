99b1c70ea3e9b6377bafe9194d38ef9bbdb642ed
HenriR21k <k2017731@kingston.ac.uk>
2022-07-01 12:12:37 +0100

    Finished adding module

--- src/components/modules/Form.js
+++ src/components/modules/Form.js
@@ -1,7 +1,7 @@
 export function Form(props) {
 
   return (
-    <form>
+    <form onSubmit={props.onSubmit}>
       {props.children}
     </form>
   )
@@ -12,7 +12,12 @@ export function FormInput(props) {
   return (
   <>
     <label for={props.name}> {props.label} </label> <br/>
-    <input type={props.type} id={props.name} name={props.name} onClick={props.onChange} placeholder={props.placeholder}/> <br />
+    <input type={props.type} id={props.name} name={props.name} onChange={props.onChange} placeholder={props.placeholder}/> <br />
+    {
+      props.error &&
+        <p>Error message: {props.error}</p>
+
+    }
   </>
   )
 }
@@ -21,8 +26,12 @@ export function FormSelect(props) {
 
   return (
   <>
-    <label for = {props.label} value = {props.value}> {props.text} </label> <br/>
-    <input type = {props.type} id={props.label} name ={props.label}/> <br />
+    <label for={props.name}>{props.label}</label> <br/>
+      <select name={props.name} id={props.id} onChange={props.onChange} > <br/>
+      {/*implement if statement to check to see which text field was clicked to display the relevant data? */}
+      {/*This approach is flawed as FormSelect is not a reusable component */}
+      {props.children}
+    </select> <br/>
   </>
   )
 }
--- src/components/modules/ModuleForm.js
+++ src/components/modules/ModuleForm.js
@@ -13,32 +13,47 @@ function ModuleForm (props) {
   // Methods -------------------------------
   const handleSubmit = (event) => {
     event.preventDefault()
-    
     props.onAdd(module);
+    props.onCancel();
   };
 
   const handleChange = (event) => {
     const updatedModule = {...module, [event.target.name]: event.target.value};
+    
+    setModule(updatedModule)
+
     console.log(event);
     console.log(event.target.name);
     console.log(event.target.value);
     console.log(updatedModule);
-    setModule(updatedModule)
   };
 
   
   
  
   // View ----------------------------------
+  const error = "cant say poo";
   return (
 
     <body>
-      <Form>
-        <FormInput name = "ModuleName" placeholder = "Enter Value" label = "Module Name" onChange={handleChange}/>
-        <FormInput name = "ModuleCode" placeholder = "Enter Value" label = "Module Code" onChange={handleChange}/>
-        <FormInput name = "ModuleLevel" label = "Module Level" onChange={handleChange}/>
-        <FormInput name = "ModuleLeaderID" label = "Module Leader ID" onChange={handleChange}/>
-        <FormInput name = "ModuleImage" placeholder = "Enter Value" label = "Module Image URL" onChange={handleChange}/>
+      <Form onSubmit={handleSubmit}>
+        <FormInput name = "ModuleName" placeholder = "Programming..." label = "Module Name" onChange={handleChange} error={null}/>
+        <FormInput name = "ModuleCode" placeholder = "CI2530..." label = "Module Code" onChange={handleChange}/>
+        <FormSelect name = "ModuleLevel" label = "Module Level" onChange={handleChange} >
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+          <option>6</option>
+          <option>7</option>
+        </FormSelect>
+        <FormSelect name = "ModuleLeaderID" label = "Module Leader ID" onChange={handleChange} >
+          <option value="k2041275">Natalie</option>
+          <option value="ku06696">Graeme</option>
+        </FormSelect>
+
+        <FormInput name = "ModuleImage" placeholder = "Enter URL..." label = "Module Image URL" onChange={handleChange}/>
 
         <Button
           className = "submitBtn"
